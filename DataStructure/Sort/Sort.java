bea0f23779ed1916b05cbc4cdb28175df668af17
Yoonji Na <yjna2316@naver.com>
2018-01-10 16:37:40 +0900

    sort

---
+++ DataStructure/Sort/Sort.java
@@ -0,0 +1,72 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class Sort {
+
+    /**
+     * Swaps a[left] and a[right]
+     */
+    public void swap (int[] a, int left, int right) {
+        int temp = a[right];
+        a[right] = a[left];
+        a[left] = temp;
+    }
+
+    /**
+     * Sorts an array using bubble sort
+     */
+    public int[] bubbleSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i ++) {
+            for (int j = 1; j < n - i; j ++) {
+                if (a[j - 1] > a[j]) {
+                    swap(a, j - 1, j);
+                }
+            }
+        }
+        return a;
+    }
+
+    /**
+     * Sorts an array using selection sort.
+     * Algorithm: first put the smallest item in the first position, then the next smallest in the second position, and so on.
+     */
+    public int[] selectionSort(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i ++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j ++) {
+                if(a[j] < a[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(a, i, minIndex);
+        }
+        return a;
+    }
+
+    /**
+     * Sorts an array using insertion sort.
+     * Algorithm: start from the second element, then slide back the elements before the second one until smaller element comes out,
+     * then start from the third element again, slide back, so on.
+     */
+    public int[] insertionSort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i ++) {
+            int key = a[i];
+            int j = i - 1;
+            for (; j >= 0 && a[j] > key; j --) {
+                a[j + 1] = a[j];
+            }
+            a[j + 1] = key;
+        }
+        return a;
+    }
+
+    public void quickSort(int[] a) {
+        int n = a.length;
+
+
+    }
+}
---
+++ DataStructure/Sort/SortTest.java
@@ -0,0 +1,33 @@
+package Sort;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class SortTest {
+
+    int[] unsorted = {5, 4, 3};
+    int[] sorted = {3, 4, 5};
+
+    Sort s = new Sort();
+
+    @Test
+    public void TestBubbleSort() {
+        assertArrayEquals(sorted, s.bubbleSort(unsorted));
+    }
+
+    @Test
+    public void TestSelectionSort() {
+        assertArrayEquals(sorted, s.selectionSort(unsorted));
+    }
+
+    @Test
+    public void TestInsertionSort() {
+        assertArrayEquals(sorted, s.insertionSort(unsorted));
+    }
+
+    @Test
+    public void TestQuickSort() {
+        assertArrayEquals(sorted, s.quickSort(unsorted);
+    }
+}
